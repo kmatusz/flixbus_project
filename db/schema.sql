d5df7c15a2dc3488e516e79171f05c87cb3ecc72
kmatusz <kmatuszelanski@gmail.com>
2019-12-14 00:17:51 +0100

    adding database experiments, proper db structure

--- db/schema.sql
+++ db/schema.sql
@@ -1,34 +1,35 @@
 create table if not exists users(
-  user_id int not null,
+  user_id int primary key,
   name varchar(30) null,
   password varchar(30) null,
   admin tinyint not null default 0,
   active tinyint not null default 1,
   time_created date null,
-  unique (name),
-  primary key (user_id)
+  unique (name)
 );
-
 create table if not exists jobs(
-  job_id int not null,
+  job_id int primary key,
   user_created int null,
   active tinyint null default 1,
   time_created datetime null,
   last_run datetime null,
   job_name varchar(20) null,
-  primary key (job_id)
+  foreign key (user_created)
+  references users (user_id)
 );
-
 create table if not exists requests(
-  request_id int not null,
+  request_id int primary key,
   job_id int null,
   start_city int null,
   end_city int null,
   date int null,
-  "column" int null,
-  primary key (request_id)
+  foreign key (job_id)
+  references jobs (job_id),
+  foreign key (start_city)
+  references cities (city_id),
+  foreign key (end_city)
+  references cities (city_id)
 );
-
 create table if not exists distances(
   start_city int not null,
   end_city int not null,
@@ -36,85 +37,48 @@ create table if not exists distances(
   primary key (
     start_city, 
     end_city
-  )
+  ),
+  foreign key (start_city)
+  references cities (city_id)
 );
-
 create table if not exists results(
-  request_id int not null,
+  request_id int auto increment,
   time_created datetime not null,
   start_city int null,
   end_city int null,
   time int null,
-  date int null,
+  date date null,
   price int null,
   changes_number int null,
   primary key (
     request_id, 
     time_created
-  )
+  ),
+  foreign key (request_id)
+  references requests (request_id),
+  foreign key (start_city)
+  references cities (city_id),
+  foreign key (end_city)
+  references cities (city_id),
+  foreign key (date)
+  references working_days (date)
 );
 create table if not exists cities(
-  city_id int not null,
-  city_name int null,
-  primary key (city_id)
+  city_id int primary key,
+  city_name int null
 );
-create table if not exists execution_log(
-  id int not null,
+create table if not exists execution_logs(
+  id int primary key,
   request_id int null,
   successful tinyint null,
   time datetime null,
   details varchar(1000) null,
-  primary key (id)
+  foreign key (request_id)
+  references requests (request_id)
 );
-create table if not exists calendar(
-  date int not null,
+create table if not exists working_days(
+  date int primary key,
   weekday int null,
-  working tinyint null,
-  primary key (date)
+  working tinyint null
 );
 
-alter table jobs
-  add foreign key (user_created)
-  references users (user_id);
-  
-alter table requests
-  add foreign key (job_id)
-  references jobs (job_id);
-  
-alter table results
-  add foreign key (request_id)
-  references requests (request_id);
-  
-alter table distances
-  add foreign key (start_city)
-  references cities (city_id);
-  
-alter table distances
-  add foreign key (end_city)
-  references cities (city_id);
-  
-alter table requests
-  add foreign key (start_city)
-  references cities (city_id);
-  
-alter table requests
-  add foreign key (end_city)
-  references cities (city_id);
-  
-alter table results
-  add foreign key (start_city)
-  references cities (city_id);
-  
-alter table results
-  add foreign key (end_city)
-  references cities (city_id);
-  
-alter table execution_log
-  add foreign key (request_id)
-  references requests (request_id);
-  
-alter table results
-  add foreign key (date)
-  references calendar (date);
-  
-create index "Index" on users(name);
